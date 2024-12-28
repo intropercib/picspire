0082583821fb9be9083f22c24a23ebdff1a13d8f
intropercib <intropercib@gmail.com>
2024-12-28 14:47:06 +0545

    any

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,46 @@ import Layout from "./Layout/Layout";
 import useAuthStore from "./components/store/useAuthStore";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { protectedRoutes, publicRoutes } from "./routes/RouteComponent";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const authUser = useAuthStore((state) => state.user);
 
   return (
-    <Layout>
-      <Routes>
-        {/* Protected Routes */}
-        {protectedRoutes.map(({ path, element: Element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <ProtectedRoute>
-                <Element />
-              </ProtectedRoute>
-            }
-          />
-        ))}
+    <>
+      <Layout>
+        <Routes>
+          {/* Protected Routes */}
+          {protectedRoutes.map(({ path, element: Element }) => (
+            <Route
+              key={path}
+              path={path}
+              element={
+                <ProtectedRoute>
+                  <Element />
+                </ProtectedRoute>
+              }
+            />
+          ))}
 
-        {/* Public Routes */}
-        {publicRoutes.map(({ path, element: Element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              authUser && path === "/signup" ? <Navigate to="/" /> : <Element />
-            }
-          />
-        ))}
-      </Routes>
-    </Layout>
+          {/* Public Routes */}
+          {publicRoutes.map(({ path, element: Element }) => (
+            <Route
+              key={path}
+              path={path}
+              element={
+                authUser && path === "/signup" ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Element />
+                )
+              }
+            />
+          ))}
+        </Routes>
+      </Layout>
+      <Analytics />
+    </>
   );
 };
 
