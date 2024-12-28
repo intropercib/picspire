d31b9c9ab89e1934db4aabc3c7028f20a8198081
intropercib <intropercib@gmail.com>
2024-12-28 16:14:58 +0545

    fix auth

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -5,6 +5,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: "picspire.vercel.app",
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
@@ -17,6 +18,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+    prompt: 'select_account'
+  });
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 export { app, auth, googleProvider, firestore, storage };
